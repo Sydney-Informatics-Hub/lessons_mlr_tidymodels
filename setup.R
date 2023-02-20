f42a1f42fd01889527b2d0f034b0ec40eae6b8fe
GiorgiaMori <giorgia.mori87@gmail.com>
2023-02-20 13:48:51 +1100

    first draft

---
+++ setup.R
@@ -0,0 +1,46 @@
+if (getRversion() < 3.6){
+  stop("You need R >= 3.6 to attend the workshop. Please update R!")
+}
+
+list_of_pkgs <- c(
+  "AmesHousing",
+  "tidyverse",
+  "readr",
+  "ggplot2",
+  "RCurl"
+  )
+
+au_repo <- "https://mirror.aarnet.edu.au/pub/CRAN/"
+
+#install installr and rtools if windows
+if (.Platform$OS.type == "windows"){
+  install.packages(c("installr"), repos = au_repo)
+  installr::install.Rtools(choose_version = F,
+                           check = T,
+                           GUI = T)
+  win_only <- c("installr")
+} else {
+  win_only <- c()
+}
+
+# run the following line of code to install the packages you currently do not have
+new_pkgs <- list_of_pkgs[!(list_of_pkgs %in% installed.packages()[,"Package"])]
+if(length(new_pkgs)) install.packages(new_pkgs,repos = au_repo)
+
+# install helper function to plot metrics
+list_of_pkgs <- c(list_of_pkgs,
+                  win_only)
+
+#check everything is installed and write out an error message if it ain't.
+missing_pkg <- list_of_pkgs[!(list_of_pkgs %in% installed.packages()[,"Package"])]
+if(length(missing_pkg)) {
+  print("=======================================================")
+  print("Packages which didn't install properly:")
+  print(missing_pkg)
+  print("Try to install them again or ask a helper for assistance.")
+  print("=======================================================")
+} else {
+  print("=======================================================")
+  print("All packages installed properly! :)")
+  print("=======================================================")
+}
