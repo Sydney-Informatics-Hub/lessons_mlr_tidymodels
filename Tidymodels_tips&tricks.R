3113fb16125e09d5ba11c922c25f5aa432b4b5eb
GiorgiaMori <giorgia.mori87@gmail.com>
2023-02-24 13:57:50 +1100

    dataset2

---
+++ Tidymodels_tips&tricks.R
@@ -0,0 +1,9 @@
+#https://usemodels.tidymodels.org
+devtools::install_github("tidymodels/usemodels")
+
+#The usemodels package is a helpful way of quickly creating code snippets to fit models using the tidymodels framework.
+#Given a simple formula and a data set, the use_* functions can create code that appropriate for the data (given the model).
+#The package includes these templates:
+ls("package:usemodels", pattern = "use_")
+
+
